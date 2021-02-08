339dc776333f4a992723577de2179aa6214e4eb4
bobokkk <mint48579@gmail.com>
2021-02-08 17:42:55 +0900

    Upload

--- code/panoptic_Seg.py
+++ code/panoptic_Seg.py
@@ -5,6 +5,8 @@ from unrealcv import client
 import sys
 import io
 from PIL import Image
+import subprocess
+import time
 
 from detectron2.config import get_cfg
 from detectron2 import model_zoo
@@ -13,36 +15,51 @@ from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog
 
 
-def read_png(image):
-    '''
-    Args :
-	res (bytes)
-
-    Return :
-	np.ndarray : an image of shape (H, W, C) (in BGR order).
-	This is the format used by OpenCV.
-    '''
-    image = Image.open(io.BytesIO(image))
-    return np.asarray(image)
+def play(path):
+    return subprocess.Popen(path, shell=True)
 
 
-if __name__ == '__main__':
+def setup_cfg():
     # load config from file and command-line arguments
     cfg = get_cfg()
 
-    model = os.getenv('MODEL_CONFIG', 'mask_rcnn_R_50_FPN_3x.yaml')
-    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/" + model))
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+    cfg.freeze()
+    return cfg
 
-    predictor = DefaultPredictor(cfg)
 
-    # 게임 바이너리와 연결
+def read_png(res):
+    '''
+    Input is bytes.
+
+    Return array
+    '''
+    img = Image.open(io.BytesIO(res))
+    return np.asarray(img)
+
+
+if __name__ == '__main__':
+    # Start game binary
+    pro = play('./RealisticRendering_RL_3.10/RealisticRendering.sh')
+
+    # Wait time to connect
+    time.sleep(2)
+
+    # Connecting to a game binary
     client.connect()
 
-    # 게임 바이너리와 연결 확인
+    # check game binary connections
     if not client.isconnected():
         print('UnrealCV server is not running.')
         sys.exit(-1)
+
+    cfg = setup_cfg()
+
+    predictor = DefaultPredictor(cfg)
+
+    # get metadata
+    metadata = MetadataCatalog.get(cfg.DATASETS.TRAIN[0])
 
     # 키보드 'Q'가 눌릴 때까지 게임 바이너리 화면을 가져와 지속적으로 화면 갱신
     while cv2.waitKey(1) != ord('q'):
@@ -57,8 +74,6 @@ if __name__ == '__main__':
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             outputs = predictor(img)
 
-            # get metadata
-            metadata = MetadataCatalog.get(cfg.DATASETS.TRAIN[0])
             # visualise
             v = Visualizer(img, metadata=metadata, scale=1.2)
             v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
